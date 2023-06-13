4bcd7578e54aaca02d296a91b5fdee1223be55b9
GUIQUERRO Nathaniel <nathaniel.g@azet.sk>
2023-06-13 09:01:18 +0200

    Début d'initialisation de la navbar fix conflit main.jsx avec erwwann

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,14 @@ import Navbar from './components/Navbar/Navbar';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>test</div>,
+    element: <Navbar/>,
   }
  ]);
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
