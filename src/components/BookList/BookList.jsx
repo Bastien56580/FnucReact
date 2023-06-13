4bcf4540358b68d785f7bad10f568ab958b01195
qlaps <qlapassas@gmail.com>
2023-06-13 15:10:35 +0200

    Admin screens WIP Need to do the CSS

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,3 +1,75 @@
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
 export default function BookList() {
-	return <></>;
+	const books = [
+		{
+			title: 'Livre A',
+			author: 'Auteur A',
+			summary: 'un résumé',
+			coverPage: 'url',
+			price: '2,00€',
+		},
+		{
+			title: 'Livre B',
+			author: 'Auteur B',
+			summary: 'un résumé',
+			coverPage: 'url',
+			price: '2,00€',
+		},
+		{
+			title: 'Livre C',
+			author: 'Auteur C',
+			summary: 'un résumé',
+			coverPage: 'url',
+			price: '2,00€',
+		},
+	];
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Titre</th>
+					<th>Auteur</th>
+					<th>Résumé</th>
+					<th>Image</th>
+					<th>Prix</th>
+					<th></th>
+					<th>
+						<AddCircleIcon />
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{books.map((element, index) => {
+					return (
+						<tr key={element + '-' + index}>
+							<td key={element.title + '-' + index}>
+								{element.title}
+							</td>
+							<td key={element.author + '-' + index}>
+								{element.author}
+							</td>
+							<td key={element.summary + '-' + index}>
+								{element.summary}
+							</td>
+							<td key={element.coverPage + '-' + index}>
+								{element.coverPage}
+							</td>
+							<td key={element.price + '-' + index}>
+								{element.price}
+							</td>
+							<td key={'update-' + index}>
+								<EditIcon />
+							</td>
+							<td key={'delete-' + index}>
+								<DeleteIcon />
+							</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
 }
--- src/components/ClientList/ClientList.jsx
+++ src/components/ClientList/ClientList.jsx
@@ -1,3 +1,61 @@
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
 export default function ClientList() {
-	return <></>;
+	const clients = [
+		{
+			firstName: 'Jeff',
+			lastName: 'Van Der Doele',
+			email: 'jeff@mail.com',
+		},
+		{
+			firstName: 'Raul',
+			lastName: 'Vamonos',
+			email: 'raul@mail.com',
+		},
+		{
+			firstName: 'Sofia',
+			lastName: 'Ben Belamni',
+			email: 'sofia@mail.com',
+		},
+	];
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Prénom</th>
+					<th>Nom</th>
+					<th>Mail</th>
+					<th></th>
+					<th>
+						<AddCircleIcon />
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{clients.map((element, index) => {
+					return (
+						<tr key={element + '-' + index}>
+							<td key={element.firstName + '-' + index}>
+								{element.firstName}
+							</td>
+							<td key={element.lastName + '-' + index}>
+								{element.lastName}
+							</td>
+							<td key={element.email + '-' + index}>
+								{element.email}
+							</td>
+							<td key={'update-' + index}>
+								<EditIcon />
+							</td>
+							<td key={'delete-' + index}>
+								<DeleteIcon />
+							</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
 }
