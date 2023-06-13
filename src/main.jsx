13f4bc4ed0fbd602a05f6004e83b1b9a374d2b42
GUIQUERRO Nathaniel <nathaniel.g@azet.sk>
2023-06-13 10:09:38 +0200

    ajout bootstrap et navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import $ from 'jquery';
+import Popper from 'popper.js';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Search from "./view/Search/Search"
 
-import Navbar from './components/Navbar/Navbar';
+
+
+
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navbar/>,
+    element: <Search/>,
   }
  ]);
 
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
