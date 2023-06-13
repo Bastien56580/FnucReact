8e9777aa91219d2a8ac3e18d322309f92ba8cd73
Ernesto <92538198+QuentinLaps@users.noreply.github.com>
2023-06-13 15:12:18 +0200

    Screen/admin (#10)
    * Admin screens init
    
    * Admin screens WIP Need to do the CSS

---
+++ src/view/Admin/Admin.jsx
@@ -0,0 +1,11 @@
+import PanelAdmin from '../../components/PanelAdmin/PanelAdmin';
+import Navbar from '../../components/Navbar/Navbar';
+
+export default function Admin() {
+	return (
+		<>
+			<Navbar />
+			<PanelAdmin />
+		</>
+	);
+}
