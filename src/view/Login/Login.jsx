00bdf72fe0d64cc4d2c304931d01285a95417f9d
GUIQUERRO Nathaniel <nathaniel.g@azet.sk>
2023-06-13 13:13:21 +0200

    navbar avancé reste a implementé la connexion

--- src/view/Login/Login.jsx
+++ src/view/Login/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/SignIn/SignIn'
+import Navbar from '../../components/Navbar/Navbar'
+
 
 export default function Login() {
   return (
-    <SignIn/>
+    <div>
+      <Navbar/>
+      <SignIn/>
+    </div>
   )
 }
