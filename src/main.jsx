aebe80b9e91bde6bf9430957d95ab2df7e9a67ff
GUIQUERRO Nathaniel <nathaniel.g@azet.sk>
2023-06-13 09:01:18 +0200

    ajout boootstrap et simple navbar

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,11 @@ const router = createBrowserRouter([
   }
  ]);
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
 )
