05fea8738621c9cb0ad4aa8ffcafe1226fd12c0a
GUIQUERRO Nathaniel <nathaniel.g@azet.sk>
2023-06-14 12:10:58 +0200

    Ajout req axios, reste id client a faire Order.jsx

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,76 +1,93 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function Order({ book }) {
-    const price = book.price;
-    const stock = book.stock;
-    const id = book.id;
+	const price = book.price;
+	const stock = book.stock;
+	const id = book.id;
 
-    //I set TVA to 20%, because why not
-    const tauxTVA = "0.2";
+	//I set TVA to 20%, because why not
+	const tauxTVA = '0.2';
 
-    //all this we get from the form
-    const [quantity, setQuantity] = useState("");
-    const [TVA, setTVA] = useState("");
-    const [HT, setHT] = useState("");
-    const [TTC, setTTC] = useState("");
+	//all this we get from the form
+	const [quantity, setQuantity] = useState('');
+	const [TVA, setTVA] = useState('');
+	const [HT, setHT] = useState('');
+	const [TTC, setTTC] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //here we need to do the post request when the back is ready to post our order,
-        //in the meanwhile there is just an alert
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		//here we need to do the post request when the back is ready to post our order,
+		//in the meanwhile there is just an alert
 
-        alert(`Submitting:
-        ID: ${id}
-        Price: ${price}
-        Stock: ${stock}
-        TVA Rate: ${tauxTVA}
-        Quantity: ${quantity}
-        TVA: ${TVA}
-        HT: ${HT}
-        TTC: ${TTC}`);
-    }
+        let current_date = new Date().toLocaleString() + ""
+		// Create user data object
+		const orderData = {
+			id_book: id,
+			id_customer: 0, //TODO : Find user id with authentication system
+			quantity: quantity,
+			order_date: current_date,
+		};
 
-    return (
-        <div className="container">
-            <div className="mt-4">
-                <p>Prix (unité) : {price}</p>
-                <p>Stock : {stock}</p>
-                <p>Taux de TVA : {tauxTVA}</p>
-                <input
-                    type="text"
-                    className="form-control mb-2"
-                    placeholder="Quantité"
-                    value={quantity}
-                    onChange={(e) => setQuantity(e.target.value)}
-                />
-                <input
-                    type="text"
-                    className="form-control mb-2"
-                    placeholder="TVA"
-                    value={TVA}
-                    onChange={(e) => setTVA(e.target.value)}
-                />
-                <input
-                    type="text"
-                    className="form-control mb-2"
-                    placeholder="HT"
-                    value={HT}
-                    onChange={(e) => setHT(e.target.value)}
-                />
-                <input
-                    type="text"
-                    className="form-control mb-2"
-                    placeholder="TTC"
-                    value={TTC}
-                    onChange={(e) => setTTC(e.target.value)}
-                />
-                <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Valider"
-                    onClick={handleSubmit}
-                />
-            </div>
-        </div>
-    )
+		// Send a POST request to create a user
+		axios
+			.post('https://apimysql-1-r1261081.deta.app/orders/', orderData, {
+				withCredentials: true,
+			})
+			.then((response) => {
+				// Handle successful response
+				if (response.data.id) {
+					toast.success('La commande est bien réalisé !'); // Display success toast message
+				} else {
+					toast.error(response.data.detail); // Display error toast message with details
+				}
+			})
+			.catch((error) => {
+				// Handle error response
+				toast.error(error.response.data.detail); // Display error toast message with details
+			});
+	};
+
+	return (
+		<div className="container">
+			<div className="mt-4">
+				<p>Prix (unité) : {price}</p>
+				<p>Stock : {stock}</p>
+				<p>Taux de TVA : {tauxTVA}</p>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="Quantité"
+					value={quantity}
+					onChange={(e) => setQuantity(e.target.value)}
+				/>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="TVA"
+					value={TVA}
+					onChange={(e) => setTVA(e.target.value)}
+				/>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="HT"
+					value={HT}
+					onChange={(e) => setHT(e.target.value)}
+				/>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="TTC"
+					value={TTC}
+					onChange={(e) => setTTC(e.target.value)}
+				/>
+				<input
+					type="submit"
+					className="btn btn-primary"
+					value="Valider"
+					onClick={handleSubmit}
+				/>
+			</div>
+		</div>
+	);
 }
--- src/view/DetailOrder/DetailOrder.jsx
+++ src/view/DetailOrder/DetailOrder.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 import Order from '../../components/Order/Order.jsx';
 import BookDetail from '../../components/BookDetail/BookDetail.jsx';
@@ -8,10 +10,24 @@ import mockB from "./mock/mockBook.json";
 
 export default function DetailOrder() {
     const [myData, setMyData] = useState(mockB);
+    const { id } = useParams(); // Récupération de la valeur 'id' depuis l'URL
     useEffect(() => {
         // Axios request or fetch the book from the id info and setMyData
         // In the meantime, we are using the mock
-    }, []);
+        axios
+			.get('https://apimysql-1-r1261081.deta.app/books/'+id, {
+				withCredentials: true,
+			})
+			.then((response) => {
+				// Handle successful response
+				setMyData(response.data);
+			})
+			.catch((error) => {
+				// Handle error response
+				toast.error(error.response.data.detail); // Display error toast message with details
+			});
+
+    }, [id]);
 
     return (
         <div className="DetailOrder">
