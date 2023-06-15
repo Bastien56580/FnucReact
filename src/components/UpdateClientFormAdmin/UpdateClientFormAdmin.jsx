0a468a2aa3cdcf5ba77de28118e817e143568af1
Linz Pierre <pierre.linz@orange.fr>
2023-06-15 11:37:16 +0200

    quality

--- src/components/UpdateClientFormAdmin/UpdateClientFormAdmin.jsx
+++ src/components/UpdateClientFormAdmin/UpdateClientFormAdmin.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 
