7d854c26acfab51727882da8dda5eed45265b42d
Bastien56580 <bastien.lesueur56@gmail.com>
2023-06-15 15:22:55 +0200

    Menu Drop

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,54 +4,59 @@ import axios from 'axios';
 export default function Search() {
   const [keywords, setKeywords] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get('https://apimysql-1-r1261081.deta.app/keywords/', {
-				withCredentials: true,
-			})
-			.then((response) => {
-				// Handle successful response
-				setKeywords(response.data);
-			})
-			.catch((error) => {
-				// Handle error response
-				toast.error(error.response.data.detail); // Display error toast message with details
-			});
-	}, []);
-     const handleSubmit = () =>{}
+  useEffect(() => {
+    axios
+      .get('https://apimysql-1-r1261081.deta.app/keywords/', {
+        withCredentials: true,
+      })
+      .then((response) => {
+        // Handle successful response
+        setKeywords(response.data);
+      })
+      .catch((error) => {
+        // Handle error response
+        toast.error(error.response.data.detail); // Display error toast message with details
+      });
+  }, []);
+  const handleSubmit = () => { }
 
   return (
     <>
-    {console.log(keywords)}
-    <div className="container mt-5">
-      <h2>Liste des mots-clés</h2>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Liste Mots-Clés</th>
-          </tr>
-        </thead>
-        <tbody>
-          {keywords.map((item) => (
-            <tr key={item}>
-              <td><KeywordItem word={item.label}/></td>
-              <td>
-              </td>
+      {console.log(keywords)}
+      <div className="container mt-5">
+        <h2>Liste des mots-clés</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Liste Mots-Clés</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      
-      { <input type="submit" value="Rechercher" onClick={handleSubmit} />}
-    </div>
-  
+          </thead>
+          <tbody>
+            {keywords.map((item) => (
+              <tr key={item}>
+                <td><KeywordItem word={item.label} /></td>
+                <td>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <select name="option" id="search-option">
+          <option value="">--Option de recherche--</option>
+          <option value="in">Dans la liste</option>
+          <option value="with">A la fois</option>
+          <option value="without">Sauf</option>
+        </select>
+        {<input type="submit" value="Rechercher" onClick={handleSubmit} />}
+      </div>
+
     </>
   )
 }
 
-function KeywordItem({word}) {
-  const [checked,setChecked] = useState(false);
-  return <><button onClick={() => setChecked(!checked)}>{checked ? '☑':'☐'}</button><ul>{word}</ul></>
+function KeywordItem({ word }) {
+  const [checked, setChecked] = useState(false);
+  return <><button onClick={() => setChecked(!checked)}>{checked ? '☑' : '☐'}</button><ul>{word}</ul></>
 }
-          
+
