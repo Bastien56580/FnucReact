9a6ad228acb47d05cf5b5a0bdb51a798a733b726
GUIQUERRO Nathaniel <nathaniel.g@azet.sk>
2023-06-15 11:52:33 +0200

    Ajout bootstrap sur les pages conect & inscription

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 export default function SignIn() {
   const [login, setLogin] = useState("");
@@ -6,14 +6,20 @@ export default function SignIn() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`j'envois login : ${login}, et passsword : ${password}`)
+    alert(`Sending login: ${login}, and password: ${password}`);
   }
-  
+
   return (
-    <div>
-        <input type="email" placeholder="Email" value={login} onChange={(e) => setLogin(e.target.value)}/>
-        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <input type="submit" value="Valider" onClick={handleSubmit} />
+    <div className="d-flex mt-5 justify-content-center" style={{ height: "100vh" }}>
+      <form className="text-center">
+        <div className="mb-3">
+          <input type="email" className="form-control" placeholder="Email" value={login} onChange={(e) => setLogin(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <input type="password" className="form-control" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit" className="btn btn-success" onClick={handleSubmit}>Valider</button>
+      </form>
     </div>
-  )
+  );
 }
