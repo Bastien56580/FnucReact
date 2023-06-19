130e17e1238a10287aaa983c8cea9901de697382
Linz Pierre <pierre.linz@orange.fr>
2023-06-19 13:34:48 +0200

    css

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -13,16 +13,36 @@ export default function Home() {
 	return (
 		<div className="Home">
 			<Navbar />
-			<CategoryList handleTopicId={handleTopicId} />
-			{topicId ? (
-				<ListBookByTopic topicId={topicId} />
-			) : (
-				<img
-					src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fecx.images-amazon.com%2Fimages%2FI%2F51qPIdBKwkL._SX258_BO1%2C204%2C203%2C200_.jpg&f=1&nofb=1&ipt=9a2141a13e9aca290cd235041a2a0f16ff8c9f492bf6250a978b18fea8039fc6&ipo=images"
-					alt="Talend BestSeller"
-					className="img-thumbnail"
-				/>
-			)}
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<div className="mb-3">
+							<CategoryList handleTopicId={handleTopicId} />
+						</div>
+					</div>
+					<div className="col-md-6">
+						<div className="mb-3">
+							{topicId ? (
+								<ListBookByTopic topicId={topicId} />
+							) : (
+								<div className="container mt-5 pt-5">
+									<div className="row justify-content-center">
+										<div className="col-md-8">
+											<div>
+												<img
+													src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fecx.images-amazon.com%2Fimages%2FI%2F51qPIdBKwkL._SX258_BO1%2C204%2C203%2C200_.jpg&f=1&nofb=1&ipt=9a2141a13e9aca290cd235041a2a0f16ff8c9f492bf6250a978b18fea8039fc6&ipo=images"
+													alt="Talend BestSeller"
+													className="img-thumbnail"
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
