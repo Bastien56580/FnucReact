b0fe20405056691e45948544c4c58ee7923b1d59
Linz Pierre <pierre.linz@orange.fr>
2023-06-19 10:29:32 +0200

    pushing stuff

--- src/components/Profile/ProfileGraph.jsx
+++ src/components/Profile/ProfileGraph.jsx
@@ -5,47 +5,54 @@ import { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 import randomColor from 'randomcolor';
 import axios from 'axios';
+import mockGraph from './mock/mockGraph.json'
 
 export default function ProfileGraph() {
-	const [myData, setMyData] = useState({});
+	const [myData, setMyData] = useState("");
 	const baseUrl = sessionStorage.getItem("REACT_APP_BACK_URL");
+	const mock = sessionStorage.getItem("REACT_APP_MOCK");
 
 	useEffect(() => {
-		axios
-			.get(baseUrl + '/customers/12/orders') //TODO : Change 12 to user id
-			.then((response) => {
-				const orders = response.data;
-				const bookTitles = {};
+		if (mock === "true") {
+			setMyData(mockGraph);
 
-				const fetchBookData = async () => {
-					for (const order of orders) {
-						try {
-							const bookResponse = await axios.get(
-								`https://apimysql-1-r1261081.deta.app/books/${order.id_book}`
-							);
-							const book = bookResponse.data;
+		} else {
+			axios
+				.get(baseUrl + '/customers/12/orders') //TODO : Change 12 to user id
+				.then((response) => {
+					const orders = response.data;
+					const bookTitles = {};
 
-							// Add book title and quantity to bookTitles object
-							if (!bookTitles[book.title]) {
-								bookTitles[book.title] = order.quantity;
-							} else {
-								bookTitles[book.title] += order.quantity;
+					const fetchBookData = async () => {
+						for (const order of orders) {
+							try {
+								const bookResponse = await axios.get(
+									baseUrl + `/books/${order.id_book}`
+								);
+								const book = bookResponse.data;
+
+								// Add book title and quantity to bookTitles object
+								if (!bookTitles[book.title]) {
+									bookTitles[book.title] = order.quantity;
+								} else {
+									bookTitles[book.title] += order.quantity;
+								}
+							} catch (error) {
+								toast.error(error.response.data.detail);
 							}
-						} catch (error) {
-							toast.error(error.response.data.detail);
 						}
-					}
 
-					// Update state with bookTitles object
-					setMyData(bookTitles);
-				};
+						// Update state with bookTitles object
+						setMyData(bookTitles);
+					};
 
-				// Fetch book data for each order
-				fetchBookData();
-			})
-			.catch((error) => {
-				toast.error(error.response.data.detail);
-			});
+					// Fetch book data for each order
+					fetchBookData();
+				})
+				.catch((error) => {
+					toast.error(error.response.data.detail);
+				});
+		}
 	}, []);
 
 
@@ -60,42 +67,46 @@ export default function ProfileGraph() {
 					<h5 className="card-title text-center">
 						{"Quels sont les livres que j'ai achetés et en combien d'exemplaires ?"}
 					</h5>
-					<div className="d-flex justify-content-center">
-						<div className="plot-container">
-							<Plot
-								data={[
-									{
-										type: 'bar',
-										x: xData,
-										y: yData,
-										marker: {
-											color: barColors,
+					{myData !== "" ? (
+						<div className="d-flex justify-content-center">
+							<div className="plot-container">
+								<Plot
+									data={[
+										{
+											type: 'bar',
+											x: xData,
+											y: yData,
+											marker: {
+												color: barColors,
+											},
 										},
-									},
-								]}
-								layout={{
-									autosize: true,
-									height: 500,
-									xaxis: {
-										ticktext: Array.from(
-											{ length: xData.length },
-											() => ''
-										),
-										tickvals: [],
-										title: 'Titre des livres',
-									},
-									yaxis: {
-										title: "Nombre d'exemplaires achetés",
-									},
-								}}
-								config={{
-									responsive: true,
-									displaylogo: false,
-								}}
-								className="w-100"
-							/>
+									]}
+									layout={{
+										autosize: true,
+										height: 500,
+										xaxis: {
+											ticktext: Array.from(
+												{ length: xData.length },
+												() => ''
+											),
+											tickvals: [],
+											title: 'Titre des livres',
+										},
+										yaxis: {
+											title: "Nombre d'exemplaires achetés",
+										},
+									}}
+									config={{
+										responsive: true,
+										displaylogo: false,
+									}}
+									className="w-100"
+								/>
+							</div>
 						</div>
-					</div>
+					) : (
+						<p>Loading</p>
+					)}
 				</div>
 			</div>
 			<Toaster />
--- src/components/Profile/ProfileList.jsx
+++ src/components/Profile/ProfileList.jsx
@@ -2,29 +2,31 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
-// Mock list since you don't have a backend to test with yet
 import mockList from './mock/mockList.json';
 
 export default function ProfileList() {
-    const [myData, setMyData] = useState(mockList);
+    const [myData, setMyData] = useState("");
     const baseUrl = sessionStorage.getItem("REACT_APP_BACK_URL");
+    const mock = sessionStorage.getItem("REACT_APP_MOCK");
     useEffect(() => {
-        // Axios request or fetch profile info
-        // In the meantime, we are using the mock
+        if (mock === "true") {
+            setMyData(mockList);
 
-        axios
-            .get(baseUrl + '/customers/12', { //TODO : Change 12 to customer id
-                withCredentials: true,
-            })
-            .then((response) => {
-                // Handle successful response
-                setMyData(response.data);
-            })
-            .catch((error) => {
-                // Handle error response
-                console.log(error);
-                toast.error(error.response.data.detail); // Display error toast message with details
-            });
+        } else if (mock === "false") {
+            axios
+                .get(baseUrl + '/customers/12', { //TODO : Change 12 to customer id
+                    withCredentials: true,
+                })
+                .then((response) => {
+                    // Handle successful response
+                    setMyData(response.data);
+                })
+                .catch((error) => {
+                    // Handle error response
+                    console.log(error);
+                    toast.error(error.response.data.detail); // Display error toast message with details
+                });
+        }
     }, []);
 
     return (
@@ -34,24 +36,35 @@ export default function ProfileList() {
                 <div className="card-body">
                     <h5 className="card-title text-center">Information du profile :</h5>
                     <table className="table">
-                        <tbody>
-                            <tr>
-                                <td className="fw-bold">Nom:</td>
-                                <td>{myData.firstname}</td>
-                            </tr>
-                            <tr>
-                                <td className="fw-bold">Prénom:</td>
-                                <td>{myData.lastname}</td>
-                            </tr>
-                            <tr>
-                                <td className="fw-bold">Email:</td>
-                                <td>{myData.email}</td>
-                            </tr>
-                            <tr>
-                                <td className="fw-bold">Total dépensé :</td>
-                                <td>{myData.cumulative_sales}</td>
-                            </tr>
-                        </tbody>
+                        {myData !== "" ? (
+                            <tbody>
+
+                                <tr>
+                                    <td className="fw-bold">Nom:</td>
+                                    <td>{myData.firstname}</td>
+                                </tr>
+                                <tr>
+                                    <td className="fw-bold">Prénom:</td>
+                                    <td>{myData.lastname}</td>
+                                </tr>
+                                <tr>
+                                    <td className="fw-bold">Email:</td>
+                                    <td>{myData.email}</td>
+                                </tr>
+                                <tr>
+                                    <td className="fw-bold">Total dépensé :</td>
+                                    <td>{myData.cumulative_sales}</td>
+                                </tr>
+
+                            </tbody>
+                        ) : (
+                            <tbody>
+                                <tr>
+                                    <td>Loading</td>
+                                </tr>
+                            </tbody>
+
+                        )}
                     </table>
                 </div>
             </div>
